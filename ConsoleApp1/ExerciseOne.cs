de636ece22d0cf20286ff556573bfe4f2a452eef
mariaacriollop <142675677+mariaacriollop@users.noreply.github.com>
2023-08-22 21:25:51 -0500

    ejercicio1

---
+++ ConsoleApp1/ExerciseOne.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ExerciseOne
+    {
+        public static void ExecuteExercise()
+        {
+            int a = 0;
+            int b = 0;
+            int c = 0; 
+            Console.WriteLine("Ingrese tres números enteros.");
+            a = int.Parse(Console.ReadLine());
+            b= int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            int Resultado = (a + b + c) / 3;
+            Console.WriteLine("El promedio de los números ingresados es: " + Resultado);    
+
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace ConsoleApp1
+﻿using System.Linq.Expressions;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //Menú para seleccionar que programa ejecutar 
+            //switch Case para la ejecucion de los ejercicios
+
+            Console.WriteLine("Ingrese el número del ejercicio que desea probar.");
+            int ejercicio = 0;
+            ejercicio = int.Parse(Console.ReadLine());
+
+            switch (ejercicio)
+            {
+                case 1:
+                    Console.WriteLine("Ejercicio 1");
+                    ExerciseOne.ExecuteExercise();
+                    break; 
+                case 2:
+                    Console.WriteLine("Ejercicio 2");
+                    break; 
+                case 3:
+                    Console.WriteLine("Ejercicio 3");
+                    break;
+
+
+
+
+            }
+
+            
+            
         }
     }
 }
